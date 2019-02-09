9b0235b7e9a49d548cfa0aa6969e55c61fd6a09c
Washington Añasco <wacho_jax@hotmail.com>
2019-02-09 01:50:05 -0500

    Me toco revertir el proyecto :c

--- Front-End/RaspberryFront/src/app/rutas/ruta-menu-admin/ruta-menu-admin.component.ts
+++ Front-End/RaspberryFront/src/app/rutas/ruta-menu-admin/ruta-menu-admin.component.ts
@@ -9,47 +9,20 @@ import {MenuItem} from "primeng/api";
 export class RutaMenuAdminComponent implements OnInit {
 
   items: MenuItem[];
-  itemsBoton: MenuItem[];
 
   constructor() { }
 
   ngOnInit() {
 
-
-
-
       this.items = [
-          {label: 'Gestion Usuarios', icon: 'fa fa-fw fa-book' , routerLink:['/login/menu-admin/gestion-usuarios']},
-          {label: 'Gestion Sensores', icon: 'fa fa-fw fa-bar-chart', routerLink:['/login/menu-admin/gestion-sensores']},
-          {label: 'Gestion Casa', icon: 'fa fa-fw fa-bar-chart', routerLink:['/login/menu-admin/gestion-casa']},
+          {label: 'Gestion Usuarios', icon: 'fa fa-fw fa-book' , routerLink: ['/login/menu-admin/gestion-usuarios']},
+          {label: 'Gestion Sensores', icon: 'fa fa-fw fa-bar-chart', routerLink: ['/login/menu-admin/gestion-sensores']},
+          {label: 'Gestion Casa', icon: 'fa fa-fw fa-bar-chart', routerLink: ['/login/menu-admin/gestion-casa']},
           {label: 'Notificaciones', routerLink: ['/login/menu-usuario/notificaciones'] }
       ];
 
-      this.itemsBoton = [
-          {label: 'Abrir', icon: 'pi pi-refresh', command: () => {
-                  this.abrir();
-              }},
-          {label: 'Cerrar', icon: 'pi pi-times', command: () => {
-                  this.cerrar();
-              }}
-      ];
 
   }
-
-
-    abrir() {
-      console.log('ssh pi@172.31.103.82')
-        console.log('admin')
-       console.log('node open.js')
-
-    }
-
-
-
-
-    cerrar() {
-       console.log('node close.js')
-    }
 
 
 }
